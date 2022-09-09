5fd9e13b81e5cbf314f3d6c2c065759dfab8d843
MANZI MUVUNYI Toussaint <toussaintmanzi006@gmail.com>
2022-09-09 12:00:44 +0200

    adding a new footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,54 +1,64 @@
-import { FaFacebookF,FaTwitter,FaInstagram,FaPhoneAlt,FaEnvelope } from "react-icons/fa"; 
+import { FaFacebookF,FaTwitter,FaInstagram,FaEnvelope,FaGithub } from "react-icons/fa"; 
 
 const Footer = () => {
 return (
-    <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-        {/* <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Fabwaste</a>. All Rights Reserved.
-        </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
-            </li>
-        </ul> */}
-        <div className="flex flex-col">
-            <div><h1 className="text-3xl">Fab<span>Waste</span></h1></div>
-            <div className="">
-                <ul class=" mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-                    <li className="inline">
-                        <a href="hf" class="mr-4 hover:underline md:mr-6 ">Home</a>
-                    </li>
-                    <li className="inline mr-4 text-sm">
-                        <a href="fhr" class="mr-4 hover:underline md:mr-6">What we do</a>
-                    </li>
-                    <li  className= "inline mr-4 text-sm">
-                        <a href="bfh" class="mr-4 hover:underline md:mr-6">Contact</a>
-                    </li>
-                    <li  className= "inline mr-4 text-sm">
-                        <a href="tbnfh" class="hover:underline">Privacy</a>
-                    </li>
-                </ul>  
-            </div>
-            <div className='flex flex-row'>
-                <FaFacebookF/>      
-                <FaTwitter/>      
-                <FaInstagram/>      
-                <FaPhoneAlt/>      
-                <FaEnvelope/>      
-            </div>
-            <div><p class="text-sm text-gray-500  dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Fabwaste</a>. All Rights Reserved.
-        </p></div>
+<footer className="p-4 bg-white sm:p-6 dark:bg-gray-900">
+    <div className="md:flex md:justify-between">
+        <div className="mb-6 md:mb-0">
+            <a href="https://flowbite.com/" className="flex items-center">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" alt="FlowBite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FabWaste</span>
+            </a>
         </div>
-  
-    </footer>
+        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                    <li className="mb-4">
+                        <a href="https://flowbite.com/" className="hover:underline">FabWaste</a>
+                    </li>
+                    <li>
+                        <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                    <li className="mb-4">
+                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                    </li>
+                    <li>
+                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                    <li className="mb-4">
+                        <a href="#" className="hover:underline">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <div className="sm:flex sm:items-center sm:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+        </span>
+        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+            <FaFacebookF className="text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+            <FaInstagram className="text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+            <FaTwitter className="text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+            <FaGithub className="text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+            <FaEnvelope className="text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+        </div>
+    </div>
+</footer>
     )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,18 +8,24 @@ const Login = () => {
 
   const [username,setUsername] = useState("");
   const [password,setPassword] =useState("");
-  const [error,setError] = useState("");
-  const [loading,setLoading] =useState("");
-  const url = ''
+  // const [error,setError] = useState("");
+  // const [loading,setLoading] =useState(true);
 
 
   const handleLogin = async () =>{
     try {
-      const response = await axios.post('url' ,{
-        "username":username,
-        "password":password
+      const response = await axios.post('https://powerful-everglades-87307.herokuapp.com/api/login' ,{
+        username,
+        password
       })
-      setLoading(false);
+
+      const token = response.data.access_token; 
+      localStorage.setItem("token",token);
+      console.log(token);
+
+    
+
+      // setLoading(false);
     } catch (error) {
       
     }
@@ -37,8 +43,8 @@ const Login = () => {
         <p className='text-xl py-3 pl-10 font-bold float-right'>Fab<span className='text-l text-greeen font-light'>Waste</span></p>
           <p className="text-center text-xl pl-2 transform translate-y-100 translate-x-9 mb-5 text-black">Welcome back</p>
           <form className="absolute text-center mt-100  mx-20" onSubmit = {handleLogin}>
-            <input type="text" placeholder="Enter email or username" className="w-full h-40 text-sm bg-f3 rounded px-5 mb-2"  onChange ={(e) => (e.target.value)}></input>
-            <input type="text" placeholder="Enter password" className="w-full h-40 text-sm bg-f3 px-5 rounded" onChange ={(e) => (e.target.value)}></input>
+            <input type="text" placeholder="Enter email or username" value={username} onChange={(e)=> setUsername(e.target.value)} className="w-full h-40 text-sm bg-f3 rounded px-5 mb-2"></input>
+            <input type="text" placeholder="Enter password" value={password} onChange={(e)=> setPassword(e.target.value)} className="w-full h-40 text-sm bg-f3 px-5 rounded"></input>
             <button type="submit" className="bg-greeen w-full h-40 mt-5 rounded text-white">Login</button>
           </form>
           <h1 className="text-sm text-greeen transform translate-y-300 translate-x-24 mt-2">forgot password ?</h1>
